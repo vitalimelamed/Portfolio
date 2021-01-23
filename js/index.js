93e5bd50f5a9e80391506e840b1bf11621cd24aa
Vitali Melamed <49156662+vitalimelamed@users.noreply.github.com>
2021-01-23 21:30:14 +1100

    Update to styles.css and index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+// Open navbar and scroll to navbar link
 const navbarToggle = document.querySelector(".navbar-toggle");
 const navbarLinks = document.querySelectorAll(".navbar__link");
 navbarToggle.addEventListener("click", () => {
   document.body.classList.toggle("navbar-open");
 });
+// Close navbar
 navbarLinks.forEach((link) => {
   link.addEventListener("click", () => {
     document.body.classList.remove("navbar-open");
