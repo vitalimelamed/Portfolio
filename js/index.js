464850f22daa7a7d4d940b6b7722b20f325f3f21
Vitali Melamed <49156662+vitalimelamed@users.noreply.github.com>
2021-01-24 17:37:52 +1100

    Updates
    Updated HTML, updated CSS, updated JavaScript

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-// Open navbar
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const toggleButton = document.getElementsByClassName('navbar-toggle')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-
+// Open navbar
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+  navbarLinks.classList.toggle('active');
+});
+// Close navbar
+navbarLinks.addEventListener('click', () => {
+        navbarLinks.classList.remove('active');
 });
