be4c6e892e9eaf275f61631a4574206eaf2cea24
Vitali Melamed <49156662+vitalimelamed@users.noreply.github.com>
2021-04-12 18:21:46 +1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-var navbarToggle = document.querySelector(".navbar-toggle");
-var navbarList = document.querySelector(".navbar-list");
-var navbarLink = document.querySelectorAll(".navbar-link");
-  navbarToggle.addEventListener("click", function() {
-    navbarList.classList.toggle("show");
+var navbarToggle = document.querySelector('.navbar-toggle');
+var navbarMenu = document.querySelector('.navbar-list');
+var navbarLink = document.querySelectorAll('.navbar-link');
+  navbarToggle.addEventListener('click', function() {
+    navbarToggle.classList.toggle('hamburger-rotate');
+    navbarMenu.classList.toggle('menu-show');
 });
   navbarLink.forEach( function(link) {
     link.addEventListener('click', function() {
-      navbarList.classList.remove('show');
+      navbarToggle.classList.remove('hamburger-rotate');
+      navbarMenu.classList.remove('menu-show');
   });
 });
