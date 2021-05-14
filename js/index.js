ab2518540600097737992743188c5b21e8528c7d
Vitali Melamed <49156662+vitalimelamed@users.noreply.github.com>
2021-05-14 18:23:48 +1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarList = document.querySelector('.navbar-list');
 const navbarLink = document.querySelectorAll('.navbar-brand, .navbar-link');
-
+// open navbar when clicking navbar toggle
   navbarToggle.addEventListener('click', () => {
     navbarToggle.classList.toggle('navbar-open');
     navbarList.classList.toggle('navbar-show');
 });
-
-  navbarLink.forEach( (navbarLink) => {
-    navbarLink.addEventListener('click', () => {
+// close navbar when clicking a link
+  navbarLink.forEach( (link) => {
+    link.addEventListener('click', () => {
       navbarToggle.classList.remove('navbar-open');
       navbarList.classList.remove('navbar-show');
   });
